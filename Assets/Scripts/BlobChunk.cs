7ef45d4cd60ad77ef08b9a72b1ebcb8c63399bab
Nathan Sizemore <nathanrsizemore@gmail.com>
2017-04-05 03:28:18 -0400

    BlobChunks now indenpendent.

--- Assets/Scripts/BlobChunk.cs
+++ Assets/Scripts/BlobChunk.cs
@@ -24,17 +24,37 @@ using UnityEngine;
 
 public class BlobChunk : MonoBehaviour
 {
+    private const int PATH_LEN = 50;
+    private const float TRAVEL_MIN = 0.25f;
+    private const float TRAVEL_MAX = 0.75f;
+    private const float UPDATE_INTERVAL = 0.25f;
+
+
     private Vector3 origin = Vector3.zero;
     private Vector3 vector = Vector3.zero;
 
+    private int pathOffset = 0;
+    private int offsetIncrementor = 1;
+    private readonly Vector3[] path = new Vector3[PATH_LEN];
+
+    private float waited = 0.0f;
+
 
 #region MONOBEHAVIOR
-    private void Awake() { CalculateVector(); }
+    private void Awake()
+    {
+        CalculateVector();
+        GeneratePath();
+    }
 
     private void Update()
     {
-        UpdatePosition();
-        UpdateRotation();
+        waited += Time.deltaTime;
+        if (waited > UPDATE_INTERVAL)
+        {
+            UpdatePosition();
+            UpdateRotation();
+        }
     }
 #endregion
 
@@ -45,9 +65,41 @@ public class BlobChunk : MonoBehaviour
         vector = origin - transform.parent.position;
     }
 
+    private void GeneratePath()
+    {
+        float t, incrementor, distance;
+        distance = RandomDistance();
+        incrementor = distance / PATH_LEN;
+        t = 0.0f;
+
+        Vector3 end = origin + (vector * distance);
+        for (int x = 0; x < PATH_LEN; x++)
+        {
+            Vector3 point = Vector3.Lerp(origin, end, t);
+            // Vector3 point = Vector3.Slerp(origin, end, t);
+            path[x] = point;
+
+            t += incrementor;
+        }
+    }
+
+    private float RandomDistance()
+    {
+        return Random.Range(TRAVEL_MIN, TRAVEL_MAX);
+    }
+
     private void UpdatePosition()
     {
+        const int OFFSET_MIN = 0;
+        const int OFFSET_MAX = PATH_LEN - 1;
 
+        if (pathOffset == OFFSET_MIN)
+            offsetIncrementor = 1;
+        else if (pathOffset == OFFSET_MAX)
+            offsetIncrementor = -1;
+
+        transform.position = path[pathOffset];
+        pathOffset += offsetIncrementor;
     }
 
     private void UpdateRotation()
